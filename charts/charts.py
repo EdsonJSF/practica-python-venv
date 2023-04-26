2c54d4c4ca863ad6be94f6417ec0247b56253ed0
Edsonjsf <edsonjsf1990@gmail.com>
2023-04-26 10:03:03 -0400

    Gráficas en Python

--- charts/charts.py
+++ charts/charts.py
@@ -1,11 +1,26 @@
 import matplotlib.pyplot as plt
 
 
-def generate_pie_chart():
-    labels = ['A', 'B', 'C']
-    values = [200, 34, 120]
+def generate_bar_chart(country, labels, values):
+    country = country.lower()
+
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.savefig(f'./charts/imgs/{country}_bar.png')
+    plt.close()
+
+
+def generate_pie_chart(continent, labels, values):
+    continent = continent.lower().replace(" ", "_")
 
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
-    plt.savefig("pie.png")
+    plt.savefig(f'./charts/imgs/{continent}_pie.png')
     plt.close()
+
+
+if __name__ == '__main__':
+    labels = ['a', 'b', 'c']
+    values = [10, 40, 800]
+    generate_bar_chart("", labels, values)
+    generate_pie_chart("", labels, values)
