7cac321a57823cef64af14941eab2fafc7f64e50
Edsonjsf <edsonjsf1990@gmail.com>
2023-05-03 08:45:02 -0400

    Python para Backend: web server con FastAPI

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Practica Python Venv")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+      <h1>Hola mundo desde FasAPI y HTMLResponse<h1/>
+      <hr/>
+    """
+
+def run():
+    print("edson")
+
+if __name__ == '__main__':
+    run()
